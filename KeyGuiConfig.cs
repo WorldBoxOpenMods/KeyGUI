e15056b1f5f19bb4addd8765f99bf47592b8b73b
key44 <modding-contributions@keymasterer.uk>
2024-12-09 11:40:50 +0100

    version bump

--- KeyGuiConfig.cs
+++ KeyGuiConfig.cs
@@ -2,7 +2,7 @@
   public static class KeyGuiConfig {
     public const string PluginGuid = "key.worldbox.keygui";
     public const string PluginName = "KeyGUI";
-    public const string PluginVersion = "1.0.14";
+    public const string PluginVersion = "1.1.0";
 
     internal static bool DebugIsLegal = false;
   }
