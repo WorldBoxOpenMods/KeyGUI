7748e52bb3a04045c9df56abe7e4ead0f33e5b7e
key44 <modding-contributions@keymasterer.uk>
2025-03-17 15:48:04 +0100

    made it possible to scale the UI

--- KeyGuiMain.cs
+++ KeyGuiMain.cs
@@ -382,18 +382,18 @@ namespace KeyGUI {
         _rootMenu.Enabled = !_rootMenu.Enabled;
       }
       if (!global::Config.gameLoaded) return;
+      GUI.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, new Vector3(1, 1, 1));
       if (KeyGuiModConfig.Get(General.ForceOpenAllMenus)) {
         _initialForcedMenuLoadPerformed = true;
         _rootMenu.ForceLoadMenu();
-      }
-      if (!_initialForcedMenuLoadPerformed) {
+      } else if (!_initialForcedMenuLoadPerformed) {
         _initialForcedMenuLoadPerformed = true;
         _rootMenu.ForceLoadMenu();
-      }
-      if (_rootMenu.Enabled) {
+      } else if (_rootMenu.Enabled) {
         _rootMenu.LoadMenu();
         _rootMenu.LoadSubMenus();
       }
+      GUI.matrix = Matrix4x4.identity;
     }
   }
 }
