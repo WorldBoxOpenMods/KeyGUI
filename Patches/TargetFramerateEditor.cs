66e31239b36e7022590bceec245dc9ac3de222f7
key44 <modding-contributions@keymasterer.uk>
2025-08-21 15:21:27 +0200

    fixed a bug in a harmony patch

--- Patches/TargetFramerateEditor.cs
+++ Patches/TargetFramerateEditor.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 namespace KeyGUI.Patches {
   public class TargetFramerateEditor : KeyGuiPatch {
     public override Dictionary<MethodInfo, (HarmonyMethod Prefix, HarmonyMethod Postfix, HarmonyMethod Transpiler, HarmonyMethod Finalizer, HarmonyMethod IlManipulator)> Patches => new Dictionary<MethodInfo, (HarmonyMethod Prefix, HarmonyMethod Postfix, HarmonyMethod Transpiler, HarmonyMethod Finalizer, HarmonyMethod IlManipulator)> {
-      {AccessTools.PropertyGetter(typeof(Application), nameof(Application.targetFrameRate)), AsHarmonyMethods(prefix: AccessTools.Method(typeof(TargetFramerateEditor), nameof(SetTargetFramerateToOwnValue)))}
+      {AccessTools.PropertySetter(typeof(Application), nameof(Application.targetFrameRate)), AsHarmonyMethods(prefix: AccessTools.Method(typeof(TargetFramerateEditor), nameof(SetTargetFramerateToOwnValue)))}
     };
 
     internal static int TargetFramerate { get; set; } = 60;
